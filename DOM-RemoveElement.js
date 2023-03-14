4b0ce2be4fac8102bbda285aa8ce268b69806c74
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-03-14 17:50:10 +0600

    Delete DOM-RemoveElement.js

--- DOM-RemoveElement.js
+++
@@ -1,9 +0,0 @@
-const body = document.body
-
-const div = document.querySelector("div")
-
-const spanHi = document.querySelector("#hi")
-
-const spanBye = document.querySelector("#bye")
-
-spanHi.removeAttribute("title")
