fa768a86bef81353429bfa5e70f22c7a25524e54
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-03-14 17:37:10 +0600

    Add files via upload
    we need to add defer in html to change in html

---
+++ DOM-RemoveElement.js
@@ -0,0 +1,9 @@
+const body = document.body
+
+const div = document.querySelector("div")
+
+const spanHi = document.querySelector("#hi")
+
+const spanBye = document.querySelector("#bye")
+
+spanHi.removeAttribute("title")
